c96fc07d7e8f6c33239049395f029a682cb5b3a2
proma <psubrun14gmail.com>
2023-10-02 10:55:23 -0700

    commit

--- Day-01-Python if else.py
+++ Day-01-Python if else.py
@@ -1,10 +1,12 @@
-n = 33
+
+
+n = int(input("Enter your number: "))
 
 if n % 2 != 0:
     print("Weird")
-elif n >= 2 and n <= 5:
+elif 2 <= n <= 5:
     print("Not Weird")
-elif n >= 6 and n <= 20:
+elif 6 <= n <= 20:
     print("Weird")
 elif n > 20:
     print("Not Weird")
